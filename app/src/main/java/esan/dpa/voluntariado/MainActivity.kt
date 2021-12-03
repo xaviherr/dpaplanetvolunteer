88dcd47e3a2814cf91f8539334918fccc26a65f6
Xavier Herrera <xaviherr@gmail.com>
2021-12-03 14:18:21 -0500

    initial commit

---
+++ app/src/main/java/esan/dpa/voluntariado/MainActivity.kt
@@ -0,0 +1,11 @@
+package esan.dpa.voluntariado
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
